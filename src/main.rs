a456667c61fc81d7ac69414d1d8e92afee2c3c19
bodrovis <bodrovis@protonmail.com>
2023-12-16 17:39:03 +0200

    v0.0.2

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ use std::fs;
 const TARGET_DIR: &str = "screens";
 
 fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let screens_dir = args.get(1).unwrap_or(&TARGET_DIR.to_string()).to_string();
     let mut path = env::current_dir()?;
-    path.push(TARGET_DIR);
+    path.push(&screens_dir);
 
     fs::create_dir_all(path)?;
-
-    let args: Vec<String> = env::args().collect();
-    let screens_dir = args.get(1).unwrap_or(&TARGET_DIR.to_string()).to_string();
-
+    
     if let Err(error) = grab(move |e| callback(e, &screens_dir)) {
         println!("Error: {error:?}");
     }
